98abd04e5094ccb00abd27cbd8234ce76c8d2a4b
Elliot Jordan <elliot@elliotjordan.com>
2015-08-03 11:28:33 -0700

    Initial repo creation and import of existing script.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# munki-grooming
-Things that help keep your Munki clean and happy.
+# Munki grooming
+
+This is where I'll store a couple scripts that help you tidy up your [Munki](https://www.munki.org/) repository. For now, there's only one:
+
+## show_unused_munki_packages.py
+
+This script attempts to determine which package names are not "used" in your Munki repo. For example, you may have imported something into Munki once (perhaps while testing an AutoPkg recipe) but never added it to any manifests. Therefore, it's just taking up space.
+
+The methodology used by the script is as follows:
+
+1. Iterate through your pkgsinfo folder, collecting all package names.
+    - Ignore any packages that are listed as an `update_for`.
+    - Ignore any `apple_update_metadata`.
+2. While doing the above, also collect any packages listed in `requires`.
+3. Collect all package names used in any manifest.
+4. Remove the results of #2 and #3 from the total list of packages in #1, and you're left with the unused packages.
+
+Known issues:
+
+- If a package is listed as an `update_for` but the thing it's updating isn't used in a manifest, it will be incorrectly be omitted from the "unused" list.
+
+
---
+++ show_unused_munki_packages.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+"""
+
+             Name:  show_unused_munki_packages.py
+      Description:  Given a path to a Munki repo, this script will tell you
+                    which package names are unused in any manifests. Useful for
+                    detecting things that were imported into Munki but never
+                    added to a manifest.
+           Author:  Elliot Jordan <elliot@lindegroup.com>
+          Created:  2015-07-30
+    Last Modified:  2015-08-03
+          Version:  1.0.7
+
+"""
+
+from xml.parsers.expat import ExpatError
+import os
+import plistlib
+from pprint import pprint
+import sys
+
+
+def process_manifest(manifest, packages_in_manifests):
+    for item in ("managed_installs", "managed_uninstalls", "managed_updates", "optional_installs"):
+        try:
+            for package in manifest[item]:
+                if package not in packages_in_manifests:
+                    packages_in_manifests.append(package)
+        except Exception:
+            continue
+    try:
+        if manifest["conditional_items"]:
+            for conditional_item in manifest["conditional_items"]:
+                process_manifest(conditional_item, packages_in_manifests)
+    except Exception:
+        pass
+
+
+def main():
+    munki_repo = raw_input(
+        "Please enter the path to your Munki repo [defaults to /Users/Shared/munki_repo]: ")
+
+    if munki_repo == "":
+        munki_repo = "/Users/Shared/munki_repo"
+
+    packages_in_repo = []
+    requirements_in_repo = []
+    for dirpath, dirnames, filenames in os.walk(os.path.join(munki_repo, "pkgsinfo")):
+        for dirname in dirnames:
+            # Skip directories that start with a period (e.g. ".git")
+            if dirname.startswith("."):
+                dirnames.remove(dirname)
+        for filename in filenames:
+            # Skip files that start with a period (e.g. ".DS_Store")
+            if filename.startswith("."):
+                continue
+            filepath = os.path.join(dirpath, filename)
+            try:
+                pkginfo = plistlib.readPlist(filepath)
+                try:
+                    if pkginfo["name"] not in packages_in_repo:
+                        if "update_for" not in pkginfo:
+                            if "installer_type" not in pkginfo:
+                                packages_in_repo.append(pkginfo["name"])
+                            elif pkginfo["installer_type"] != "apple_update_metadata":
+                                packages_in_repo.append(pkginfo["name"])
+                    if "requires" in pkginfo:
+                        for requirement in pkginfo["requires"]:
+                            if requirement not in requirements_in_repo:
+                                requirements_in_repo.append(requirement)
+                except KeyError:
+                    continue
+            except ExpatError:
+                print >> sys.stderr, "Could not parse %s" % os.path.join(
+                    dirpath, filename)
+
+    packages_in_manifests = []
+    for dirpath, dirnames, filenames in os.walk(os.path.join(munki_repo, "manifests")):
+        for dirname in dirnames:
+            # Skip directories that start with a period (e.g. ".git")
+            if dirname.startswith("."):
+                dirnames.remove(dirname)
+        for filename in filenames:
+            # Skip files that start with a period (e.g. ".DS_Store")
+            if filename.startswith("."):
+                continue
+            filepath = os.path.join(dirpath, filename)
+            try:
+                manifest = plistlib.readPlist(filepath)
+                process_manifest(manifest, packages_in_manifests)
+            except ExpatError:
+                print >> sys.stderr, "Could not parse %s" % os.path.join(
+                    dirpath, filename)
+
+    unused_packages = list(set(packages_in_repo) - set(requirements_in_repo) - set(packages_in_manifests))
+    print "\n    UNUSED PACKAGES:\n"
+    pprint(unused_packages)
+
+if __name__ == '__main__':
+    main()
